df5dc06114bd790c1300b7d919bfc685cd07c930
dcosic1 <dcosic1@etf.unsa.ba>
2017-05-20 01:11:27 +0200

    ++

---
+++ ProjekatKino/ProjekatKino/Models/KinoDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.Entity;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace ProjekatKino.Models
+    {
+    class KinoDbContext : DbContext
+        {
+        public DbSet<Korisnik> korisnici { get; set; }
+        public DbSet<Uposlenik> uposlenici { get; set; }
+        public DbSet<Film> filmovi { get; set; }
+
+
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+            {
+            string databaseFilePath = "ProjekatKinoBaza.db";
+            try
+                {
+                //za tačnu putanju gdje se nalazi baza uraditi ovdje debug i procitati Path
+                databaseFilePath = Path.Combine(ApplicationData.Current.LocalFolder.Path,
+                databaseFilePath);
+                }
+            catch (InvalidOperationException) { }
+            //Sqlite baza
+            optionsBuilder.UseSqlite($"Data source={databaseFilePath}");
+            }
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+            {
+            //jedno od polja je image da se zna šta je zapravo predstavlja byte[]
+            //modelBuilder.Entity<Korisnik>().Property(p => p.Slika).HasColumnType("image");
+            //modelBuilder.Entity<Uposlenik>().Property(p => p.Slika).HasColumnType("image");
+            }
+
+        }
+    }
---
+++ ProjekatKino/ProjekatKino/Models/Korisnik.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatKino.Models
+    {
+    public class Korisnik : Osoba
+        {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private int idKorisnika; // primary key
+        private int brojKreditneKartice;
+        private DateTime expDate;
+        private string imeVlasnikaKartice;
+        private string prezimeVlasnikaKartice;
+
+        public Korisnik (string ime, string prezime, string adresa, string email, string username, string password, DateTime datumRodjenja, int idKorisnika, int brojKreditneKartice, DateTime expDate, string imeVlasnikaKartice, string prezimeVlasnikaKartice)
+                : base(ime, prezime, adresa, email, username, password, datumRodjenja)
+            {
+            this.idKorisnika = idKorisnika;
+            this.brojKreditneKartice = brojKreditneKartice;
+            this.expDate = expDate;
+            this.imeVlasnikaKartice = imeVlasnikaKartice;
+            this.prezimeVlasnikaKartice = prezimeVlasnikaKartice;
+            }
+
+        public int IdKorisnika
+            {
+            get
+                {
+                return idKorisnika;
+                }
+
+            set
+                {
+                idKorisnika = value;
+                }
+            }
+
+        public int BrojKreditneKartice
+            {
+            get
+                {
+                return brojKreditneKartice;
+                }
+
+            set
+                {
+                brojKreditneKartice = value;
+                }
+            }
+
+        public DateTime ExpDate
+            {
+            get
+                {
+                return expDate;
+                }
+
+            set
+                {
+                expDate = value;
+                }
+            }
+
+        public string ImeVlasnikaKartice
+            {
+            get
+                {
+                return imeVlasnikaKartice;
+                }
+
+            set
+                {
+                imeVlasnikaKartice = value;
+                }
+            }
+
+        public string PrezimeVlasnikaKartice
+            {
+            get
+                {
+                return prezimeVlasnikaKartice;
+                }
+
+            set
+                {
+                prezimeVlasnikaKartice = value;
+                }
+            }
+        }
+    }
